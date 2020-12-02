8a95bc2793bb1fd740145709090021feed4386b5
ZAJU97 <46608680+ZAJU97@users.noreply.github.com>
2020-12-02 16:09:40 +0100

    Update node.py

--- MinerHtml/node.py
+++ MinerHtml/node.py
@@ -1,31 +1,19 @@
-import scrapy 
+import requests
+import logging
+from bs4 import BeautifulSoup
+from minemeld.ft.basepoller import BasePollerFT
 
-class Miner(scrapy.Spider): 
-    name = "planszoPajak" 
-    start_urls = 'https://boardgamegeek.com/browse/boardgame?sort=rank&amp;rankobjecttype=subtype&amp;rankobjectid=1'
-    download_delay = 5 
-    def parse(self, response): 
-        zestaw=response.css(".collection_table").xpath(".//tr") 
-        for item in zestaw: 
-            i = {} 
-            i['tytul'] = item.css(".collection_objectname a::text").extract_first() 
-            i['url'] = item.css(".collection_objectname a::attr(href)").extract_first() 
-            request = scrapy.Request("https://boardgamegeek.com"+str(i['url']), 
-                             callback=self.parse_page2) 
-            request.meta['i'] = i 
-            yield request 
-        next_page = response.xpath("//a[@title='next page']").css("a::attr(href)").extract_first() 
-        if next_page and '3' not in str(next_page): 
-            yield scrapy.Request( 
-                response.urljoin(next_page), 
-                callback=self.parse 
-            )          
-    def parse_page2(self, response): 
-        i = response.meta['i'] 
-        script = str(response.xpath("//html/head/script/text()").extract_first()) 
-        x = script.find('"description":') 
-        y = script.find('"wiki":') 
-        i['overview'] = script[(x+15):(y-2)].replace(',','') 
-        yield i
+LOG = logging.getLogger(__name__)
 
 
+class Miner(BasePollerFT):
+
+  url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s'
+  response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+  html = response.content
+
+  soup = BeautifulSoup(html)
+  table = soup.find('tbody', attrs={'class': 'stripe'})
+
+  for row in table.findAll('tr'):
+    print row.prettify()
